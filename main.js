16a4a5fde45fab490165a98e28d68e92b142ea44
Orel-Viktor <OrelViktor93@gmail.com>
2022-10-06 16:54:52 +0300

    kak ya ustal

--- main.js
+++ main.js
@@ -1,6 +1,51 @@
-import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+function Tasks(input, output) {
+  this.input = document.querySelector('.js--input')
+  this.output = document.querySelector('.js--myTasks')
+  const labelDescription = document.querySelector('.js--label-description')
+ 
+  
+
+
+
+  this.addText = (event) => {
+    event.preventDefault()
+    const value = this.input.value
+    this.input.value = ''
+    const items = this.createTemplate(value)
+    this.output.insertAdjacentHTML('beforeend', items)
+    document.querySelector('.js--delete').addEventListener('click', (e)=>{
+      this.output.removeChild(labelDescription)
+    })
+  }
+
+ 
+  this.createTemplate = (description) => {
+    return (
+      ` <label class ="js--label-description}" >
+        <input type="checkbox" class ='js--complete'>
+        <div class="description">${description}</div>
+        <button  class ="js--delete">Удалить</button>
+        </label>  
+  `
+    )
+  }
+  
+  
+
+}
+
+
+const tasks = new Tasks('.js--input', '.js--myTasks')
+document.querySelector('.js--form').addEventListener('submit', tasks.addText)
+
+
+
+
+
+
+
+
+
+
+
