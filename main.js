a09d85d70a60f01ef010fc4dd16bc0cf0d3c5756
Orel-Viktor <OrelViktor93@gmail.com>
2022-10-06 19:24:50 +0300

    redy

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 function Tasks(input, output) {
   this.input = document.querySelector('.js--input')
   this.output = document.querySelector('.js--myTasks')
-  const labelDescription = document.querySelector('.js--label-description')
+  
  
   
 
@@ -12,23 +12,37 @@ function Tasks(input, output) {
     event.preventDefault()
     const value = this.input.value
     this.input.value = ''
-    const items = this.createTemplate(value)
-    this.output.insertAdjacentHTML('beforeend', items)
-    document.querySelector('.js--delete').addEventListener('click', (e)=>{
-      this.output.removeChild(labelDescription)
-    })
+    const label = this.createTemplate(value)
+    this.output.appendChild(label)
+   
   }
 
  
   this.createTemplate = (description) => {
-    return (
-      ` <label class ="js--label-description}" >
-        <input type="checkbox" class ='js--complete'>
-        <div class="description">${description}</div>
-        <button  class ="js--delete">Удалить</button>
-        </label>  
-  `
-    )
+    const div = document.createElement('div')
+    const input = document.createElement('input')
+    input.setAttribute('type','checkbox')
+    const text = document.createElement('div')
+    const button =  document.createElement('button')
+    text.innerText = description
+    button.innerText = 'Удалить'
+    button.addEventListener('click',function(){
+      div.remove()
+    })
+    input.addEventListener('change',function(){
+      /* if(input.checked){
+        text.classList.add('done')
+      } else {
+          text.classList.remove('done')
+      } */
+      text.style.textDecoration = input.checked ? 'line-through' : 'none'
+
+    })
+    div.appendChild(input)
+    div.appendChild(text)
+    div.appendChild(button)
+    return div
+    
   }
   
   
